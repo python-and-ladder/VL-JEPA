d6a999ec4cedd4c05c38807eb524e647232a0e15
vaishnavivaishnavi7715@gmail.com <vaishnavivaishnavi7715@gmail.com>
2026-08-11 20:49:47 +0530

    Add UV setup with PyTorch test

---
+++ test_pytorch.py
@@ -0,0 +1,12 @@
+import torch
+
+print("PyTorch version:", torch.__version__)
+
+x = torch.tensor([1, 2, 3])
+y = torch.tensor([4, 5, 6])
+
+print("x:", x)
+print("y:", y)
+print("x + y:", x + y)
+
+print("PyTorch is working successfully!")
